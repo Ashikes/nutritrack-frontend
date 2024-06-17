92f7c9e17defe2c63cf290ec413d6dc4f6b805af
Ashicodestudios <ashik1149@gmail.com>
2024-06-17 18:00:56 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-//   baseURL: 'https://nutritrack-backend.onrender.com/api',
-  baseURL: 'http://127.0.0.1:5000/api',
+  baseURL: 'https://nutritrack-backend.onrender.com/api',
 });
 const headers = {
     'Authorization': "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2NmRlZWJmMjQxOWU2ZjU3ZjNhYjc4MSIsImlhdCI6MTcxODYyNzAwMywiZXhwIjoxNzE4NjMwNjAzfQ.n_rqM-xkgcCEzfPTC1R8fbE-c6k7PkTp3YN6OI_X0b4", // Replace with your actual token
